23855c0674d1c8d3cbbdedf06f79be4310aeee7e
Ryan Hakes <ryanhakes@Ryans-MacBook-Pro-2.local>
2026-01-31 23:08:56 -0500

    Increase default logo size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,6 +4,6 @@ type LogoProps = Omit<ImageProps, "src" | "alt"> & {
   alt?: string;
 };
 
-export default function Logo({ alt = "Summit Sisters", width = 160, height = 64, ...props }: LogoProps) {
+export default function Logo({ alt = "Summit Sisters", width = 220, height = 80, ...props }: LogoProps) {
   return <Image src="/logo.svg" alt={alt} width={width} height={height} {...props} />;
 }
